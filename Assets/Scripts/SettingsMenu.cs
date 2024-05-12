cc71d25d463d3b82d21206420322ca09d7f2b67e
Rafay Junaid <rafooaj11@gmail.com>
2024-05-12 18:24:19 +0500

    menu finished

--- Assets/Scripts/SettingsMenu.cs
+++ Assets/Scripts/SettingsMenu.cs
@@ -5,14 +5,17 @@ public class SettingsMenu : MonoBehaviour
     public Transform audSettings;
     public Transform vidSettings;
     public Transform mainSettings;
+    public Transform controlSettings;
     bool isAud = false;
     bool isVid = false;
+    bool isContr = false;
 
     // Start is called before the first frame update
     void Start()
     {
         isAud = false;
         isVid = false;
+        isContr= false;
     }
 
     // Update is called once per frame
@@ -22,17 +25,29 @@ public class SettingsMenu : MonoBehaviour
             audSettings.gameObject.SetActive(false);
         if (!isVid)
             vidSettings.gameObject.SetActive(false);
+        if (!isContr)
+            controlSettings.gameObject.SetActive(false);
     }
     public void OnAudioButton()
     {
         isAud = true;
         audSettings.gameObject.SetActive(true);
+        controlSettings.gameObject.SetActive(false);
         mainSettings.gameObject.SetActive(false);
     }
     public void OnVideoButton()
     {
         isVid = true;
         vidSettings.gameObject.SetActive(true);
+        controlSettings.gameObject.SetActive(false);
+        mainSettings.gameObject.SetActive(false);
+    }
+    public void OnControlButton()
+    {
+        isContr = true;
+        controlSettings.gameObject.SetActive(true);
+        vidSettings.gameObject.SetActive(false);
+        audSettings.gameObject.SetActive(false);
         mainSettings.gameObject.SetActive(false);
     }
     public void OnAudBackButton()
@@ -45,6 +60,12 @@ public class SettingsMenu : MonoBehaviour
     {
         isVid = false;
         vidSettings.gameObject.SetActive(false);
+        mainSettings.gameObject.SetActive(true);
+    }
+    public void OnContrBackButton()
+    {
+        isContr = false;
+        controlSettings.gameObject.SetActive(false);
         mainSettings.gameObject.SetActive(true);
     }
 }
